bfda1798d0ff6d0ba14cfc38df9c8319b85eaf28
Beukens <beukensgaetan@gmail.com>
2025-01-20 08:08:53 +0100

    **Initialize Spring Boot project with Maven wrapper**
    Set up a Spring Boot project with Maven build tools, including wrapper scripts, `application.yml`, and `pom.xml`. Configure dependencies for web, JPA, testing, security, and mailing. Add `.gitignore` and `.gitattributes` to enhance version control management.

---
+++ src/test/java/be/yapock/ccf/CcfApplicationTests.java
@@ -0,0 +1,13 @@
+package be.yapock.ccf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
