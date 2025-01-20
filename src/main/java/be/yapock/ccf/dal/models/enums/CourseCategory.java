1de0ba7e9849787966aa53da694f43a36e3999e2
Beukens <beukensgaetan@gmail.com>
2025-01-20 09:27:19 +0100

    **Add enums for DogSize, RaceGroup, and CourseCategory**
    Introduce enums `DogSize`, `RaceGroup`, and `CourseCategory` to standardize values related to dog sizes, race groups, and course categories. These additions improve data consistency and simplify future expansions of related functionality.

---
+++ src/main/java/be/yapock/ccf/dal/models/enums/CourseCategory.java
@@ -0,0 +1,7 @@
+package be.yapock.ccf.dal.models.enums;
+
+public enum CourseCategory {
+    OBEDIANCE,
+    AGILITY,
+    RING
+}
---
+++ src/main/java/be/yapock/ccf/dal/models/enums/DogSize.java
@@ -0,0 +1,7 @@
+package be.yapock.ccf.dal.models.enums;
+
+public enum DogSize {
+    SMALL,
+    AVERAGE,
+    BIG
+}
